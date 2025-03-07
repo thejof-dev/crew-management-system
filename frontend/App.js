c872d930ecc6e1d2ff383fe98c7634643590a73c
thejof-dev <jof@xoqp.co>
2025-03-07 18:11:07 +0000

    Initial commit - Crew Management System

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,6 @@
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
+import './styles.css';
 
 const App = () => {
   const [clients, setClients] = useState([]);
